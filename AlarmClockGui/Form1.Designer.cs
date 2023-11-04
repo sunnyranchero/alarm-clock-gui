53105b5f163eac51a6bc142aef276241658675bb
sunnyranchero <106451955+sunnyranchero@users.noreply.github.com>
2023-11-04 19:45:32 -0400

    Add files via upload

---
+++ AlarmClockGui/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace AlarmClockGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button7 = new Button();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(18, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Hour";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(96, 6);
+            button2.Name = "button2";
+            button2.Size = new Size(72, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Minute";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(174, 6);
+            button3.Name = "button3";
+            button3.Size = new Size(72, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Second";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(252, 6);
+            button4.Name = "button4";
+            button4.Size = new Size(72, 29);
+            button4.TabIndex = 3;
+            button4.Text = "On";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(330, 6);
+            button5.Name = "button5";
+            button5.Size = new Size(72, 29);
+            button5.TabIndex = 4;
+            button5.Text = "Off";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(330, 42);
+            button6.Name = "button6";
+            button6.Size = new Size(72, 29);
+            button6.TabIndex = 5;
+            button6.Text = "Timer";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(159, 39);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(100, 50);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "AM/PM";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(57, 26);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(43, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "PM";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(14, 26);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(44, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.Text = "AM";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(18, 95);
+            button7.Name = "button7";
+            button7.Size = new Size(384, 29);
+            button7.TabIndex = 7;
+            button7.Text = "SNOOZE";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Silver;
+            label1.Location = new Point(18, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 23);
+            label1.TabIndex = 8;
+            label1.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(422, 131);
+            Controls.Add(label1);
+            Controls.Add(button7);
+            Controls.Add(groupBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Alarm Clock";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button7;
+        private Label label1;
+    }
+}
---
+++ AlarmClockGui/Form1.cs
@@ -0,0 +1,20 @@
+namespace AlarmClockGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
